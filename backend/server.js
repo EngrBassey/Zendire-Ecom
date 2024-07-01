d09155517fc6b282041ccaac933062c7ea86e17f
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-07-01 09:21:04 +0100

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const cookieParser = require('cookie-parser')
 
 const DatabaseClient = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 
 const setupSwagger = require('./config/swagger');
 
@@ -20,6 +23,8 @@ app.use(cookieParser());
 
 app.use('/uploads', express.static('uploads'));
 app.use('/api/product', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 
 setupSwagger(app);
