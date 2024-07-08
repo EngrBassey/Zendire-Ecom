f78c4b9e91b658f5b11ccb8da4a2319fe9cb4cc7
EngrBassey <basseyleads001@gmail.com>
2024-07-08 19:55:37 +0100

    worked on resoponsiveness

--- Frontend/menu-app/src/components/Navbar/navbar.js
+++ Frontend/menu-app/src/components/Navbar/navbar.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState
 import { Link } from 'react-router-dom';
 import classes from './navbar.module.css';
 import logo from '../../assert/Zendir-logo.png';
 import { FaCartShopping } from "react-icons/fa6";
+import { FaBars, FaTimes } from "react-icons/fa"; // Import icons for toggle button
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State to manage mobile menu
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
-        <div className={classes.position} >
+        <div className={classes.position}>
             <div className={classes.navbar}>
-                <div className={classes.navbarbox1} >
+                <div className={classes.navbarbox1}>
                     <img src={logo} alt='Zendir-logo' />
                 </div>
-                <div className={classes.navbarbox2} >
+                <button className={classes.toggleButton} onClick={toggleMobileMenu}>
+                    {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+                </button>
+                <div className={`${classes.navbarbox2} ${isMobileMenuOpen ? classes.showMenu : ''}`}>
                     <Link to='/'>Home</Link>
                     <Link to=''>Categories</Link>
                     <Link to=''>Products</Link>
-                    <Link to=''>Patners</Link>
+                    <Link to=''>Partners</Link>
                     <Link to=''>Shop</Link>
                     <Link to=''>Contact</Link>
-                    <div>
+                    <div className={classes.iconLinks}>
                         <Link to="/cart">
                             <FaCartShopping />
                         </Link>
@@ -27,7 +37,6 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
